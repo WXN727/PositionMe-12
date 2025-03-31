ea87161b8918e6228dbd77efcb27df2ba0445bc1
WXN <wxn030727@gmail.com>
2025-03-31 19:33:48 +0100

    Better UI display across different fragments
    Fixed crashes due to WIFI update
    
    Made fused arrow default indication and fused path default path

--- app/src/main/java/com/openpositioning/PositionMe/MainActivity.java
+++ app/src/main/java/com/openpositioning/PositionMe/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements Observer {
         Toolbar toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
         toolbar.showOverflowMenu();
-        toolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.primaryBlue));
+        //toolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.primaryBlue));
         toolbar.setTitleTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
 
         // Set up back action
